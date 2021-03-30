b2186e7548e8d6e67c6f0caba5f2f21661e5e700
nuttareeb <nuttaree.b@gmail.com>
2021-03-29 22:44:37 -0700

    Add logic for early_join_merged_run

--- pmj.py
+++ pmj.py
@@ -11,9 +11,7 @@ def writefile(filename, data):
 def quicksort(arr, key):
     if len(arr) < 2:
         return arr
-    
-    # print("arr:", arr)
-
+        
     pivot = arr[0, key]
     pivot_list = arr[0]
     left = np.array([x for x in arr if x[key] < pivot])
@@ -35,28 +33,31 @@ class SweepArea:
         self.key = key
 
     def insert(self, v):
-        self.X = self.X.append(v)
+        self.X.append(v)
     
-    def query(self, v, v_key):
+    def query(self, v, v_key, except_sequence_i = -1):
         # remove elements that less than v
+
         i = 0
-        while self.X[i][self.key] < v[v_key]:
+        while i < len(self.X) and self.X[i][self.key] < v[v_key]:
             self.X = self.X[1:]
             i += 1
         
         result = []
 
-        i = 0
-        while self.X[i][self.key] == v[v_key]:
-            result.append((self.X[i], v))
-        
+        for i in range(len(self.X)):
+            if except_sequence_i == i:
+                continue
+            if self.X[i][self.key] == v[v_key]:
+                result.append((self.X[i], v))
+
         return result
 
 
 def early_join_init_run(sorted_r, sorted_s, key_R, key_S):
     RES = []
-    R_m = SweepArea([], key_R)
-    S_m = SweepArea([], key_S)
+    R_M = SweepArea([], key_R)
+    S_M = SweepArea([], key_S)
     
     while len(sorted_r) > 0 or len(sorted_s) > 0:
         r = []
@@ -67,18 +68,52 @@ def early_join_init_run(sorted_r, sorted_s, key_R, key_S):
             s = sorted_s[0]
         
         if len(sorted_s) == 0 or (len(sorted_r) > 0 and r[key_R] <= s[key_S]):
-            R_m.insert(r)
-            RES.append(S_m.query(r, key_R))
+            R_M.insert(r)
+            RES.append(S_M.query(r, key_R))
             sorted_r = sorted_r[1:]
         else:
-            S_m.insert(s)
-            RES.append(R_m.query(s, key_S))
+            S_M.insert(s)
+            RES.append(R_M.query(s, key_S))
             sorted_s = sorted_s[1:]
 
     return RES
 
 def early_join_merged_run(q, sorted_r, sorted_s, key_R, key_S):
-    return (sorted_r, sorted_s)
+    RES = []
+    R_M = SweepArea([], key_R)
+    S_M = SweepArea([], key_S)
+
+    R_m = [r for r, s in q]
+    S_m = [s for r, s in q]
+    
+    while any([len(r) > 0 for r in R_m]) or any([len(s) > 0 for s in S_m]):
+        r = []
+        s = []
+
+        r_i = -1
+        s_j = -1
+
+        for i, rm in enumerate(R_m):
+            if len(rm) > 0 and (len(r) == 0 or rm[0][key_R] < r[key_R]):
+                r = rm[0]
+                r_i = i
+        for j, sm in enumerate(S_m):
+            if len(sm) > 0 and (len(s) == 0 or sm[0][key_S] < s[key_S]):
+                s = sm[0]
+                s_j = j
+        
+        if s_j == -1 or (r_i > -1 and r[key_R] <= s[key_S]):
+            R_M.insert(r)
+            RES.append(S_M.query(r, key_R, r_i))
+            R_m = R_m[:r_i] + R_m[r_i + 1:]
+            sorted_r.append(r)
+        else:
+            S_M.insert(s)
+            RES.append(R_M.query(s, key_S, s_j))
+            S_m = S_m[:s_j] + S_m[s_j + 1:]
+            sorted_s.append(s)
+
+    return RES
 
 """
 input: 
@@ -126,7 +161,9 @@ def pmj(R, S, mem_size, max_fan_in, key_R, key_S):
         writefile("sorted_r.txt", sorted_r)
         writefile("sorted_s.txt", sorted_s)
 
-        Q.append((sorted_r, sorted_s)) 
+        Q.append((sorted_r, sorted_s))
+
+    print(np.array(res))
 
 R = loaddata("r.txt")
 # print(R)
@@ -135,5 +172,4 @@ S = loaddata("s.txt")
 # print(S)
 
 pmj(R, S, 8, 10, 0, 0)
-
 
